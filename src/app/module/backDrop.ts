0ad52c07f753108044890a73c79d17203f9ce9ed
praveen-snow <praveenkumar.snow@gmail.com>
2017-04-16 21:12:49 -0500

    my first angular 2.0 app

--- src/app/module/backDrop.ts
+++ src/app/module/backDrop.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from '../superClass/backDrop';
 
 @NgModule({
   declarations: [
--- src/app/superClass/backDrop.ts
+++ src/app/superClass/backDrop.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: '../components/backDrop/backDrop.html',
+  styleUrls: ['../components/backDrop/backDrop.css']
 })
 export class AppComponent {
   title = 'app works!';
