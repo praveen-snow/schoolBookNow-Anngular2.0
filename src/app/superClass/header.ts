ed4aaa770c606466ca9a72741608f6cfc4632d80
praveen-snow <praveenkumar.snow@gmail.com>
2017-04-16 23:45:52 -0500

    nested component with require for rendering

--- src/app/superClass/header.ts
+++ src/app/superClass/header.ts
@@ -2,9 +2,11 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: '<nav class="sbw-header-navigator">{{textTest}}</nav>',
+    template: require('../components/header/header.html'),
     styleUrls: ['../components/header/header.css']
 })
 export class HeaderComponent{
     textTest = 'Hello World';
 }
+
+//'<nav class="sbw-header-navigator"><h1>{{textTest}}</h1></nav>',
