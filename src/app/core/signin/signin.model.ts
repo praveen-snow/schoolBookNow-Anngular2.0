e24fa0a6bf785404084c44e81b58f3c06118bfd3
praveen <praveen.chundi13@gmail.com>
2017-04-24 20:55:54 -0500

    sign in changes

--- src/app/core/signin/signin.model.ts
+++ src/app/core/signin/signin.model.ts
@@ -2,3 +2,8 @@ export class User {
     userId: string;
     passCode: string;
 }
+
+export class SignIn {
+  userId: string;
+  passCode: string;
+}
--- src/app/core/signin/signin.ts
+++ src/app/core/signin/signin.ts
@@ -2,10 +2,10 @@
  * Created by rahul on 4/20/17.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 import { SignInServices } from './signin.service';
 import { AppAPI } from 'app/api/app.api';
-import { User } from './signin.model';
+import { User, SignIn } from './signin.model';
 
 @Component({
   moduleId: module.id,
@@ -19,10 +19,27 @@ export class SigninComponent implements OnInit {
   textTest = 'School Book';
   userProfileImg = '../../assets/images/praveen.jpg';
   userProfileName = 'Praveen Chundi';
+  @Input() signIn: SignIn;
+  editUser: SignIn = <SignIn>{};
   users: User[];
   constructor(private singInService: SignInServices) { }
   ngOnInit() {
     this.singInService.checkUser()
       .subscribe(users => this.users = users);
+  }
+  private setEditCharacter(signIn: SignIn) {
+    if (signIn) {
+      this.signIn = signIn;
+      //this.editCharacter = this.entityService.clone(this.character);
+    }
+  }
+  getIn() {
+    const userDetails = this.editUser;
+    if (userDetails.userId === null || userDetails.passCode === null) {
+      return;
+    }
+    console.log(userDetails);
+    // this.characterService.updateCharacter(character)
+    //   .subscribe(() => this.toastService.activate(`Successfully saved ${character.name}`));
   }
 }
